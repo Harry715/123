bbdfd866d663f15450512ea123c211a0752f9b2d
xiao20020715 <xiao20020715@gmail.com>
2024-12-20 22:46:25 +0800

    firstView

---
+++ src/app/views/first-view/first-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-view',
+  imports: [],
+  templateUrl: './first-view.component.html',
+  styleUrl: './first-view.component.scss'
+})
+export class FirstViewComponent {
+
+}
